94603040062341df9b90d66b1232563cbca3bdf6
Dj8PoWer <r.segard@hotmail.fr>
2020-02-13 00:34:56 +0100

    il faut bosser les enfants

--- GoodEnoughGame/Assets/Scripts/PlayerManager.cs
+++ GoodEnoughGame/Assets/Scripts/PlayerManager.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
     [SerializeField]
-    int speed;
+    int speed = 0;
+
+    private PhotonView PV;
 
     SpriteRenderer renderer;
 
     // Start is called before the first frame update
     void Start()
     {
+        PV = GetComponent<PhotonView>();
         renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Deplacement();
+        if (PV.IsMine)
+            Deplacement();
     }
 
     void Deplacement()
