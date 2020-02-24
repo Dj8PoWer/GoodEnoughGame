b16207cdffed7dbac36fa23557e75026414fc506
Dj8PoWer <r.segard@hotmail.fr>
2020-02-24 18:01:31 +0100

    yo

--- GoodEnoughGame/Assets/Scripts/Player/Items.cs
+++ GoodEnoughGame/Assets/Scripts/Player/Items.cs
@@ -7,18 +7,18 @@ public class Items : MonoBehaviour
     private int ilvl;
     private int hpFlat;
     private float hpPercent;
-    private float damage;
+    private float globalDamage;
 
     public Items()
     {
         this.hpFlat = ilvl * 100 + 50;
         this.hpPercent = ilvl * 0.01f + 0.02f;
-        this.damage = ilvl * 0.02f + 0.05f;
+        this.globalDamage = ilvl * 0.02f + 0.05f;
     }
 
     public int HpFlat => hpFlat;
 
     public float HpPercent => hpPercent;
 
-    public float Damage => damage;
+    public float GlobalDamage => globalDamage;
 }
