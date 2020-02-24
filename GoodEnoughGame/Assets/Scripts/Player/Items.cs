a57fb91deff8b02ce0727bcab77d721d3b4346a0
Dj8PoWer <r.segard@hotmail.fr>
2020-02-24 17:29:20 +0100

    ITEMS CLASS HAS BEEN CREATED OMG IM SO GOOD AT THIS

---
+++ GoodEnoughGame/Assets/Scripts/Player/Items.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour
+{
+    private int ilvl;
+    private int hpFlat;
+    private float hpPercent;
+    private float damage;
+
+    public Items()
+    {
+        this.hpFlat = ilvl * 100 + 50;
+        this.hpPercent = ilvl * 0.01f + 0.02f;
+        this.damage = ilvl * 0.02f + 0.05f;
+    }
+
+    public int HpFlat => hpFlat;
+
+    public float HpPercent => hpPercent;
+
+    public float Damage => damage;
+}
