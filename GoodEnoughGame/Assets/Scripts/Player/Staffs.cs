77a3f55ab0223f23513249a48d329725911a7791
Dj8PoWer <r.segard@hotmail.fr>
2020-02-25 14:10:13 +0100

    stats selector

--- GoodEnoughGame/Assets/Scripts/Player/Staffs.cs
+++ GoodEnoughGame/Assets/Scripts/Player/Staffs.cs
@@ -1,12 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Staffs : Weapons
 {
-    private float movespeed;
+    private float moveSpeed;
+    private int baseDmg;
 
-    List<statsType> possibleStats = new List<statsType>
+    private List<(statsType, float)> stats = new List<(statsType, float)>();
+
+    private List<statsType> possibleStats = new List<statsType>
     {
         statsType.globalDamage,
         statsType.hpFlat,
@@ -41,6 +45,64 @@ public class Staffs : Weapons
         attackSpeed += Ilvl * 0.01f;
         castSpeed += Ilvl * 0.01f;
 
-        movespeed = (Ilvl * 0.01f + 0.02f) * -1;
+        moveSpeed = (Ilvl * 0.01f + 0.02f) * -1;
+        
+        GetRandomStats();
     }
+
+    private void GetRandomStats()
+    {
+        for (int i = 0; i <= Random.Range(1, possibleStats.Count - 1); i++)
+        {
+            int j = Random.Range(0, possibleStats.Count - 1);
+            switch (possibleStats[j])
+            {
+                case statsType.hpFlat:
+                    stats.Add((statsType.hpFlat, hpFlat));
+                    break;
+                case statsType.hpPercent:
+                    stats.Add((statsType.hpFlat, hpPercent));
+                    break;
+                case statsType.moveSpeed:
+                    stats.Add((statsType.moveSpeed, moveSpeed));
+                    break;
+                case statsType.attackSpeed:
+                    stats.Add((statsType.attackSpeed, attackSpeed));
+                    break;
+                case statsType.castSpeed:
+                    stats.Add((statsType.castSpeed, castSpeed));
+                    break;
+                case statsType.globalDamage:
+                    stats.Add((statsType.globalDamage, globalDamage));
+                    break;
+                case statsType.physicalDmgFlat:
+                    stats.Add((statsType.physicalDmgFlat, physicalDmgFlat));
+                    break;
+                case statsType.physicalDmgPercent:
+                    stats.Add((statsType.physicalDmgPercent, physicalDmgPercent));
+                    break;
+                case statsType.fireDamageFlat:
+                    stats.Add((statsType.fireDamageFlat, fireDamageFlat));
+                    break;
+                case statsType.waterDamageFlat:
+                    stats.Add((statsType.waterDamageFlat, waterDamageFlat));
+                    break;
+                case statsType.airDamageFlat:
+                    stats.Add((statsType.airDamageFlat, airDamageFlat));
+                    break;
+                case statsType.fireDamagePercent:
+                    stats.Add((statsType.fireDamagePercent, fireDamagePercent));
+                    break;
+                case statsType.waterDamagePercent:
+                    stats.Add((statsType.waterDamagePercent, waterDamagePercent));
+                    break;
+                case statsType.airDamagePercent:
+                    stats.Add((statsType.airDamagePercent, airDamagePercent));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
 }
