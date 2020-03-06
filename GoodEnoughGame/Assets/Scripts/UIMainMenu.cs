64cd45bce59cc88f6b7f80b8975ec61c07803feb
quentin laurent <quentin.laurent@epita.fr>
2020-03-06 15:32:36 +0100

    Main menu + options menu + buttons textures

--- GoodEnoughGame/Assets/Scripts/UIMainMenu.cs
+++ GoodEnoughGame/Assets/Scripts/UIMainMenu.cs
@@ -8,7 +8,8 @@ public class UIMainMenu : MonoBehaviour
 {
     public void QuitGame()
     {
-
+        Debug.Log("QUIT GAME");
+        Application.Quit();
     }
 
     public void JoinOnlineLooby()
