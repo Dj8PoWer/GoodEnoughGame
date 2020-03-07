4111f7bc161ad1ed020e37abde72959139b0343b
W3RTY <alexis_de_carvalho@yahoo.com>
2020-03-07 21:00:22 +0000

    Staff completed

---
+++ GoodEnoughGame/Assets/Scripts/Weapon/Projectile.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float time = 2f;
+    [SerializeField] private float speed;
+    public Vector2 mousePos;
+    public Rigidbody2D projectile;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        projectile = GetComponent<Rigidbody2D>();
+
+        Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(0, 180) - 90);
+        Vector3 vect = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        Debug.Log(vect);
+        Vector3 rotateVector = rotation * vect;
+        Debug.Log(rotateVector);
+
+        projectile.AddForce(rotateVector * speed * 50)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentVector= new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y);
+        projectile.AddForce(currentVector * Time.deltaTime * speed);
+
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ GoodEnoughGame/Assets/Scripts/Weapon/Staff.cs
@@ -0,0 +1,46 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Staff : MonoBehaviour
+{
+    public float attackSpeed;
+    private float cooldown;
+
+    private Animator animator;
+    public Transform projectilePos;
+    public Transform projectileRot;
+    public GameObject projectile;
+
+    private PhotonView PV;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PV.IsMine)
+        {
+            cooldown -= Time.deltaTime;
+            if (cooldown <= 0 && Input.GetKey(KeyCode.Mouse0))
+            {
+                PV.RPC("RPC_Attack", RpcTarget.All, projectilePos.position, projectileRot.rotation, (Vector2)(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+                cooldown = attackSpeed;
+            }
+        }
+    }
+    
+    [PunRPC]
+    void RPC_Attack(Vector3 pos, Quaternion rot, Vector2 mousePos)
+    {
+        var Object = Instantiate(projectile, pos, rot);
+        var projectil = Object.GetComponent<Projectile>();
+        projectil.mousePos = mousePos;
+    }
+}
