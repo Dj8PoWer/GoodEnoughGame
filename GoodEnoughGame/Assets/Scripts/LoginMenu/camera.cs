5b30d0cdc193c8b6a11b08437db915811c4656d3
AloisH <heloir.alois@live.fr>
2020-05-23 15:55:34 +0200

    Add background into the loading screen

---
+++ GoodEnoughGame/Assets/Scripts/LoginMenu/camera.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, -30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
